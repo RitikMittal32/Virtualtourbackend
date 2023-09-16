c83e2b59749ec018a896b7fee3b97e79bdf295c8
RitikMittal32 <ritikmittal8068@gmail.com>
2023-09-17 00:45:25 +0530

    second commit

--- db/config.js
+++ db/config.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/virtual-tour");
 
+// Replace "your-mongodb-hostname" with the actual hostname or IP address of your MongoDB server
+const mongoURI = "mongodb://127.0.0.1:27017/virtual-tour";
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+module.exports = mongoose;
