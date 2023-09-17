353731b4b21e1aad90d30d42488c1b5c02b32095
RitikMittal32 <ritikmittal8068@gmail.com>
2023-09-17 12:45:23 +0530

    third commit

--- db/config.js
+++ db/config.js
@@ -1,21 +1,4 @@
 const mongoose = require('mongoose');
 
 // Replace "your-mongodb-hostname" with the actual hostname or IP address of your MongoDB server
-const mongoURI = "mongodb://127.0.0.1:27017/virtual-tour";
-
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
-module.exports = mongoose;
+mongoose.connect("mongodb+srv://ritikmittal8068:WfByiOZyBAY5onYG@cluster0.4reghec.mongodb.net/?retryWrites=true&w=majority")
